2279db57c25945953aef16d7f29fafcd6d46b73a
rohan <rxr105@student.bham.ac.uk>
2024-01-06 01:40:53 +0000

    Enhancing Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,66 +11,44 @@ import SettingsDropdown from '@/app/components/Settings';
 import styles from '@/app/Navbar.module.css'
 
 const Navbar = () => {
-    const {data: session} : any = useSession();
+  const { data: session }: any = useSession();
 
-    return (
-        <div className={styles.navbar}>
-            <ul className="flex justify-between m-10 item-center">
-                <div>
-                    <a href="https://www.birmingham.ac.uk/index.aspx" target="_blank">
-                        <Image src={MyImage} alt="University of Birmingham Logo" />
-                    </a>
-                </div>
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <a href="https://www.birmingham.ac.uk/index.aspx" target="_blank">
+          <Image src={MyImage} alt="University of Birmingham Logo" />
+        </a>
+      </div>
 
-                <div className="flex gap-10 items-center">
-                    {!session ? (
-                        <>
-                                <Link href="/">
-                                    <li>Home</li>
-                                </Link>
+      <ul className={styles.navLinks}>
+        {!session ? (
+          <>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/login">Login</Link></li>
+            <li><Link href="/register">Register</Link></li>
+          </>
+        ) : (
+          <>
+            <li><Link href="/home">Home</Link></li>
+            <li><Link href="/chatbot">Chatbot</Link></li>
+            <li><Link href="/history">Chat History</Link></li>
+            <li><Link href="/contact">Help</Link></li>
+          </>
+        )}
+      </ul>
 
-                            <Link href="/login">
-                                <li>Login</li>
-                            </Link>
-
-                            <Link href="/register">
-                                <li>Register</li>
-                            </Link>
-                        </>
-                    ) : (
-                        <>
-                            <Link href="/home">
-                            <li>Home</li>
-                            </Link>
-
-                            <Link href="/chatbot">
-                                <li>Chatbot</li>
-                            </Link>
-
-                            <Link href="/history">
-                                <li>Chat History</li>
-                            </Link>
-
-                            <Link href="/contact">
-                                <li> Help </li>
-                            </Link>
-
-                            <div className="flex justify-end m-10 item-right gap-8">
-                                {session.user.name}
-                                <li>
-                                    <SettingsDropdown />
-                                </li>
-                            </div>
-                        </>
-                    )}
-
-                </div>
-            </ul>
+      {session && (
+        <div className={styles.userSettings}>
+          <span>{session.user.name}</span>
+          <SettingsDropdown />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
-export default Navbar
+export default Navbar;
 
 
 
