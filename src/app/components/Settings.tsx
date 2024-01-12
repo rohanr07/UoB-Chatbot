97b6c85d9c838607c181088212ae952d65ea9e8a
rohan <rohancricket79@gmail.com>
2024-01-12 19:21:17 +0000

    Enhancing Settings button on Navbar

--- src/app/components/Settings.tsx
+++ src/app/components/Settings.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { signOut, useSession } from "next-auth/react";
+import styles from '@/app/Pages.module.css'
+import Link from "next/link";
 
 interface MenuItem {
   name: string;
@@ -59,57 +61,32 @@ const SettingsDropdown = () => {
   }, [session, session?.user?.email]);
 
   return (
-    <div className="dropdown"
+    <div className={styles.dropdown}
          onMouseEnter={() => setIsHovering(true)}
          onMouseLeave={() => setIsHovering(false)}
     >
-      <button className="dropdown-header">Settings</button>
+      <button className={styles["dropdown-header"]}>Settings</button>
        {isHovering && (
-           <ul className="dropdown-list">
+           <ul className={styles["dropdown-list"]}>
              {menuItems.map((item, index) => (
-                 <li className="dropdown-list-item"
-                     key={index}
-                     onClick={() => handleNavigation(item.route)}>
-                   {item.name}
-                 </li>
+                 item.route !== '/api/auth/signout' ? (
+              <li className={styles["dropdown-list-item"]} key={index}>
+                <Link className={styles["dropdown-list-item"]} href={item.route}>
+                  {item.name}
+                </Link>
+              </li>
+            ) : (
+              <li
+                className={styles["dropdown-list-item"]}
+                key={index}
+                onClick={() => handleNavigation(item.route)}
+              >
+                {item.name}
+              </li>
+            )
              ))}
            </ul>
        )}
-      <style jsx>{`
-        .dropdown {
-          position: relative;
-          display: inline-block;
-        }
-
-        .dropdown-header {
-          background-color: #6a40de;
-          color: black;
-          padding: 16px;
-          font-size: 16px;
-          border: none;
-          cursor: pointer;
-        }
-
-        .dropdown-list {
-          display: block;
-          position: absolute;
-          background-color: #beb065;
-          min-width: 160px;
-          box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-          z-index: 1;
-        }
-
-        .dropdown-list-item {
-          color: black;
-          padding: 12px 16px;
-          text-decoration: none;
-          display: block;
-        }
-
-        .dropdown-list-item:hover {
-          background-color: #ddd;
-        }
-      `}</style>
     </div>
   );
 };
