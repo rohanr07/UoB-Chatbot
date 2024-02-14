d657a7c54abed83dba0f1b62ab2786243120de52
rohan <rxr105@student.bham.ac.uk>
2024-02-14 21:15:25 +0000

    Added validation for Change Password screen

--- src/app/api/update-credentials/route.ts
+++ src/app/api/update-credentials/route.ts
@@ -1,9 +1,9 @@
 // pages/api/update-credentials.ts
-import type {NextApiRequest, NextApiResponse} from 'next';
+import type {NextApiRequest} from 'next';
 import User from '@/models/User';
 import bcrypt from 'bcryptjs';
 import {authenticateUser, AuthResult} from "@/utils/authentication";
-//import {NextResponse} from "next/server";
+import {NextResponse} from "next/server";
 import connect from '@/utils/mongodb';
 
 
@@ -16,45 +16,39 @@ async function streamToString(stream: any) {
     return Buffer.concat(chunks).toString('utf8');
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextApiRequest) {
     const {userEmail, session}: AuthResult = await authenticateUser(req);
-
-    await connect();
+    console.log('Inside update -credentials');
+    //await connect();
 
     if (!userEmail || !session) {
-        return res.status(401).json({error: 'User not Authenticated'});
+        //return res.status(401).json({error: 'User not Authenticated'});
+        return NextResponse.json({error: 'User not authenticated'}, {status: 401});
     }
 
-    const requestBody = await streamToString( req.body)
+    const requestBody = await streamToString(req.body)
     const parsedBody = JSON.parse(requestBody);
 
-    const { currentPassword, newPassword } = parsedBody;
-    const existingUser = await User.findOne({ email: userEmail });
+    const {currentPassword, newPassword} = parsedBody;
+    const existingUser = await User.findOne({email: userEmail});
 
-  // Check if the current password is correct with existing stored password
-  const isMatch = await bcrypt.compare(currentPassword, existingUser.password);
+    // Check if the current password is correct with existing stored password
+    const isMatch = await bcrypt.compare(currentPassword, existingUser.password);
 
-  if (!isMatch) {
-      console.log("Current Password is Incorrect")
-    return res.status(401).json({ message: 'Current Password is Incorrect' });
-  }
+    if (!isMatch) {
+        console.log("Current password is Incorrect")
+        return NextResponse.json({error: 'Current Password is incorrect'}, {status: 501});
+    }
 
-  console.log("Current Password is Correct")
+    console.log("Current Password is Correct")
 
-  // Hash the new password
-  const salt = await bcrypt.genSalt(5);
+    // Hash the new password
+    const salt = await bcrypt.genSalt(5);
 
-  // Update the user's password with the new password (hashed)
-  existingUser.password = await bcrypt.hash(newPassword, salt);
-  await existingUser.save();
+    // Update the user's password with the new password (hashed)
+    existingUser.password = await bcrypt.hash(newPassword, salt);
+    await existingUser.save();
 
-  res.status(200).json({ message: 'Password updated successfully' });
+    //res.status(200).json({ message: 'Password updated successfully' });
+    return NextResponse.json({message: 'Password updated successfully'}, {status: 200});
 }
-
-
-
-
-
-
-
-
--- src/app/change-credentials/page.tsx
+++ src/app/change-credentials/page.tsx
@@ -1,95 +1,177 @@
- "use client";
-import React, {useEffect, useState} from 'react';
-import { useRouter } from 'next/navigation';
-import {getSession, useSession} from "next-auth/react";
-import styles from '@/app/Pages.module.css'
+"use client";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from "next/navigation";
+import { getSession } from 'next-auth/react';
+import styles from '@/app/Pages.module.css';
 
 const Change = () => {
-  const { data: session } : any = useSession();
+  const [error, setError] = useState("");
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
   const router = useRouter();
+  //const { data: session, status: sessionStatus } = useSession();
 
-        useEffect(() => {
-            if (!session) {
-                // Redirect to login page if not signed in
-                console.log("User is not authenticated. Redirecting to login page.");
-                router.push("/login");
-                return;
-            } else {
-                console.log(" USER IS AUTHENTICATED ");
-            }
-        }, []);
+  useEffect(() => {
+    const fetchSession = async () => {
+        const session = await getSession();
 
-  const isFormValid = () => {
-    return (
-      (currentPassword && newPassword && newPassword === confirmNewPassword)
-    );
-  };
+        if (!session) {
+            // Redirect to login page if not signed in
+            console.log("User is not authenticated. Redirecting to login page.");
+            router.push("/login");
+            return;
+        } else {
+            console.log(" USER IS AUTHENTICATED ");
+        }
+    };
 
-  const handleCancel = () => {
-    // Go back to the previous page
-    router.back();
-  };
+     console.log("Fetching session...");
+    fetchSession();
+}, []);
 
-  const handleConfirm = async () => {
-    if (!isFormValid()) return;
-
-    try {
-    const response = await fetch('/api/update-credentials', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ currentPassword, newPassword }),
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-      console.log('Credentials updated successfully');
-      router.back();
-    } else {
-      console.error('Failed to Update Credentials', data.message);
+      const isFormValid = () => {
+          console.log(' inside isFormValid');
+    if (newPassword !== confirmNewPassword) {
+        setError('New passwords do not match');
+        return false;
     }
-  } catch (error) {
-    console.error('Error updating credentials:', error);
-  }
-  };
-
-  return (
-    <div className={styles.pageContainer}>
-      <h1 className={styles.pageTitle}>Change Password</h1>
-        <div className="bg-[#212121] p-7 rounded shadow-md w-full max-w-md">
-          <input
-            className={styles.credentialsPage}
-            type="password"
-            placeholder="Enter current password"
-            value={currentPassword}
-            onChange={(e) => setCurrentPassword(e.target.value)}
-          />
-          <input
-            className={styles.credentialsPage}
-            type="password"
-            placeholder="Enter new password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-          />
-          <input
-            className={styles.credentialsPage}
-            type="password"
-            placeholder="Confirm new password"
-            value={confirmNewPassword}
-            onChange={(e) => setConfirmNewPassword(e.target.value)}
-          />
-            <button className={styles.buttonCancel} onClick={handleCancel}>
-              Cancel
-            </button>
-            <button className={styles.button} onClick={handleConfirm} disabled={!isFormValid()}>
-              Confirm
-            </button>
-          </div>
-        </div>
-  );
+    if (currentPassword === newPassword) {
+        setError('New password cannot be same as current password');
+        return false;
+    }
+    if (newPassword.length < 7) {
+        setError('Password should be at least 7 characters long');
+        return false;
+    }
+    if (!/[A-Z]/.test(newPassword)) {
+        setError('Password should contain an uppercase character');
+        return false;
+    }
+    if (!/[a-z]/.test(newPassword)) {
+        setError('Password should contain a lowercase character');
+        return false;
+    }
+    if (!/\d/.test(newPassword)) {
+        setError('Password should contain a number');
+        return false;
+    }
+    // Clear error if all validations pass
+    setError('');
+    return true;
 };
 
-export default Change;
+      const handleCancel = () => {
+          router.back(); // Go back to the previous page
+      };
+
+      const handleConfirm = async (e: any) => {
+          console.log('AT 70');
+             e.preventDefault();
+          if (!isFormValid()) return  false;
+
+          console.log('Form is valid');
+         /* if (newPassword !== confirmNewPassword) {
+        setError('New passwords do not match');
+        return;
+    }
+      if (!/[a-z]/.test(newPassword)) {
+        setError('Password should contain a lowercase character');
+        return;
+       }*/
+          try {
+              const response = await fetch('/api/update-credentials', {
+                  method: 'POST',
+                  headers: {'Content-Type': 'application/json'},
+                  body: JSON.stringify({currentPassword, newPassword}),
+              });
+              if (response.ok) {
+                  console.log('Credentials updated successfully');
+                  setError('Credentials updated successfully');
+                  //router.back();       router.push("/login");
+              } else {
+                  // If the response is not successful, handle the error
+                  return response.json().then(data => {
+                      // Access the error message and status code
+                      const errorMessage = data.error;
+                      const statusCode = response.status;
+                      console.log(" statusCode:", statusCode)
+                      // Handle the error appropriately (e.g., display an error message)
+                      console.error(`Error ${statusCode}: ${errorMessage}`);
+                      console.error('Failed to Update Credentials', data.message);
+                      setError(`${errorMessage}`);
+
+
+                  });
+
+              }
+          } catch (error) {
+              console.error('Error updating credentials:', error);
+          }
+      };
+
+
+  return (
+<div className={styles.pageContainer}>
+    <div className={styles.credentialFields}>
+        <h1 className={styles.pageTitle}>Change Password</h1>
+        <form onSubmit={handleConfirm}>
+            <div className="bg-[#212121] p-7 rounded shadow-md w-full max-w-md">
+                <input
+                    className={styles.credentialsPage}
+                    type="password"
+                    placeholder="Enter current password"
+                    value={currentPassword}
+                    onChange={(e) => setCurrentPassword(e.target.value)}
+                />
+                <input
+                    className={styles.credentialsPage}
+                    type="password"
+                    placeholder="Enter new password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <input
+                    className={styles.credentialsPage}
+                    type="password"
+                    placeholder="Confirm new password"
+                    value={confirmNewPassword}
+                    onChange={(e) => setConfirmNewPassword(e.target.value)}
+                />
+                <button className={styles.buttonCancel} type="button"
+                        onClick={handleCancel}> {/* Use type="button" to prevent form submission */}
+                    Cancel
+                </button>
+                <button className={styles.button}
+                        type="submit"
+                >
+
+                    Confirm
+                </button>
+            </div>
+
+            <br/>
+
+        <p className={styles.passwordRules}>
+            <b>Password must contain...</b>
+        </p>
+         <ul className={styles.passwordRules}>
+                <li>At least 7 characters</li>
+                <li>An uppercase character</li>
+                <li>A lowercase character</li>
+                <li>A number</li>
+            </ul>
+
+        {error && (
+            <p className={styles.errorMessage}>{error}</p>
+        )}
+                </form>
+
+         </div>
+    </div>
+
+    );
+    };
+
+    export default Change;
